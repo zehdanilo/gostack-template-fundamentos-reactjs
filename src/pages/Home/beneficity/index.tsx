edadef868befd514c46054af963bd317b58ede32
Danilo Diógenes <zehdanilo@gmail.com>
2020-09-24 20:58:47 -0300

    Finalizando beneficios

--- src/pages/Home/beneficity/index.tsx
+++ src/pages/Home/beneficity/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-import { Container, Cards } from './styles';
+import { Container, Cards, Card, Title } from './styles';
+
+import Digital from '../../../assets/digital.svg';
+import Seguranca from '../../../assets/seguranca.svg';
+import Velocidade from '../../../assets/velocidade.svg';
 
 const Beneficity: React.FC = () => {
   return (
@@ -9,24 +13,41 @@ const Beneficity: React.FC = () => {
         <h1>Beneficios do seu CartórioMIX</h1>
 
         <Cards>
-          <ul>
-            <li>
-              <img src="#" />
-              <h1>Maior Seguranca</h1>
-              <span>Lorem Ipsum</span>
-            </li>
-            <li>
-              <img src="#" />
-              <h1>Mais Rapidez</h1>
-              <span>Lorem Ipsum</span>
-            </li>
+          <Card>
+            <img src={Seguranca} alt="Maior Segurança" />
+            <Title>
+              <h1>Maior</h1>
+              <h2>Seguranca</h2>
+            </Title>
+            <span>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.{' '}
+            </span>
+          </Card>
 
-            <li>
-              <img src="#" />
-              <h1>Mais Rapidez</h1>
-              <span>Lorem Ipsum</span>
-            </li>
-          </ul>
+          <Card>
+            <img src={Velocidade} alt="Mais Rapidez" />
+            <Title>
+              <h1>Mais</h1>
+              <h2>Rapidez</h2>
+            </Title>
+            <span>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.{' '}
+            </span>
+          </Card>
+
+          <Card>
+            <img src={Digital} alt="Tudo Digital" />
+            <Title>
+              <h1>Tudo</h1>
+              <h2>Digital</h2>
+            </Title>
+            <span>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.{' '}
+            </span>
+          </Card>
         </Cards>
       </Container>
     </>
--- src/pages/Home/beneficity/styles.ts
+++ src/pages/Home/beneficity/styles.ts
@@ -1,28 +1,53 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
   margin: 0 auto;
   height: 628px;
 
   h1 {
     display: flex;
-    justify-content: space-around;
+    justify-content: center;
+
+    font-size: 13px;
+    line-height: 24px;
+    letter-spacing: 0.16em;
+    text-transform: uppercase;
+    margin-top: 98px;
+    margin-bottom: 50px;
+    color: #6c757c;
   }
 `;
 
 export const Cards = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: center;
+`;
 
-  ul {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    list-style-type: none;
+export const Card = styled.div`
+  width: 300px;
+  margin: 0px auto;
+
+  span {
+    font-size: 16px;
+    line-height: 24px;
+    color: #bdbdbd;
+  }
+`;
+
+export const Title = styled.div`
+  margin: 20px auto;
+  text-transform: uppercase;
+
+  h1 {
+    justify-content: left;
+    font-size: 32px;
+    margin: 0px auto;
+    color: #30b78e;
   }
 
-  li {
+  h2 {
+    font-size: 32px;
+    color: #043c35;
   }
 `;
